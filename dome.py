8d0f0b7905a8d1d8696eccbbda8314db6064c4cf
Marta Feriani <marta.celeste.feriani@gmail.com>
2016-12-20 18:03:50 +0000

    dome.py: rotating edgeloops

--- dome.py
+++ dome.py
@@ -1,27 +1,33 @@
-#importing Python Maya commands
+# importing Python Maya commands
 import maya.cmds as cmds
 
-#creating a sphere and storing its name
-cmds.polySphere(name = 'dome_right', radius = 1, subdivisionsX = 10, subdivisionsY = 10)
+# creating a sphere and storing its name
+cmds.polySphere(name='dome_right', radius=1, subdivisionsX=10, subdivisionsY=10)
 name = 'dome_right'
 
-# get the selection of the object - you can do some error checking, if not polygon or if component selection...
+# get the selection of the object
 listFaces = []
-#    loop for each face...
+# loop for each face...
 
 for i in xrange(89, 80, -1):
-    listFaces.append(name + '.f[' + str(i) +']')
-        
+    listFaces.append(name + '.f[' + str(i) + ']')
+
 for i in xrange(19, 0, -1):
-    listFaces.append(name + '.f[' + str(i) +']')
+    listFaces.append(name + '.f[' + str(i) + ']')
 listFaces = cmds.ls(listFaces, fl=True)
 cmds.delete(listFaces)
 cmds.delete(name + '.f[61]')
 cmds.delete(name + '.f[0]')
 
-#fill bottom hole
-cmds.select( name + '.e[1]', r=True )
+# fill bottom hole
+cmds.select(name + '.e[1]', r=True)
 cmds.polyCloseBorder()
 
-#    clean the history
+# rotate edgeloop
+for i in range(1, 7):
+    angle = 36 * i
+    edgeLoop = str(name) + '.e[' + str(i) + '0:' + str(i) + '9]'
+    cmds.rotate(0, str(angle) + 'deg', 0, str(edgeLoop))
+
+# clean the history
 cmds.delete(name, ch=1)
