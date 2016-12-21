b43637363f1cad6a64c2575d8117dd5a95125e56
Marta Feriani <marta.celeste.feriani@gmail.com>
2016-12-21 18:55:36 +0000

    dome.py: now scales correctly

--- dome.py
+++ dome.py
@@ -26,8 +26,23 @@ cmds.polyCloseBorder()
 # rotate edgeloop
 for i in range(1, 7):
     angle = 36 * i
-    edgeLoop = str(name) + '.e[' + str(i) + '0:' + str(i) + '9]'
+    edgeLoop = name + '.e[' + str(i) + '0:' + str(i) + '9]'
     cmds.rotate(0, str(angle) + 'deg', 0, str(edgeLoop))
+
+# scaling edgeloops
+scaleValue = 1
+step = 0.2
+for i in range(4, 7):
+    edgeLoop = name + '.e[' + str(i) + '0:' + str(i) + '9]'
+    scaleValue -= step
+    print step
+    print scaleValue
+    # scale only x, z value so that they remain on the same height
+    cmds.scale(scaleValue, 1, scaleValue, str(edgeLoop))
+    step += 0.1
+
+# moving tip of the dome
+cmds.polyMoveVertex(name + '.vtx[70]', ty=0.2)
 
 # clean the history
 cmds.delete(name, ch=1)
