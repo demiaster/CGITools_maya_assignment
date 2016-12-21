95a2d4d4d2e195eb2fb7f7717e6576a028ad14bb
Marta Feriani <marta.celeste.feriani@gmail.com>
2016-12-21 22:49:04 +0000

    dome.py: now scales and moves to (wrong) position

--- dome.py
+++ dome.py
@@ -35,8 +35,6 @@ step = 0.2
 for i in range(4, 7):
     edgeLoop = name + '.e[' + str(i) + '0:' + str(i) + '9]'
     scaleValue -= step
-    print step
-    print scaleValue
     # scale only x, z value so that they remain on the same height
     cmds.scale(scaleValue, 1, scaleValue, str(edgeLoop))
     step += 0.1
@@ -44,5 +42,18 @@ for i in range(4, 7):
 # moving tip of the dome
 cmds.polyMoveVertex(name + '.vtx[70]', ty=0.2)
 
+# scaling whole dome to fit the tower
+scale = 0.52
+cmds.scale(scale, scale, scale, str(name))
+
+# retrieving position of top of the left tower
+towerName = 'turret_wall'
+leftTop = cmds.pointPosition(towerName + '.pt[214]', w=True)
+
+# moving dome pivot to one of its bottom points
+newPivotPos = cmds.pointPosition(name + '.pt[6]', w=True)
+cmds.move(newPivotPos[0], newPivotPos[1], newPivotPos[2], name + '.translatePivot', a=True)
+#print str(cmds.pointPosition(name + '.pt[214]', w=True))
+cmds.move(leftTop[0], leftTop[1], leftTop[2], name)
 # clean the history
 cmds.delete(name, ch=1)
